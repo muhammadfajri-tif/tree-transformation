55ee4cfa6aa0b0decbbef0590daac68b281db911
muhammadfajri-tif <149763217+muhammadfajri-tif@users.noreply.github.com>
2024-04-25 15:41:08 +0700

    init project base

---
+++ main.c
@@ -0,0 +1,9 @@
+#include "./includes/boolean.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  boolean isFinished = false;
+  printf("Current Tubes: %i ", isFinished);
+
+  return 0;
+}
