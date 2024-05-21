d55a301d2f8f6cb8f4e0be0176aef51403cdc6b9
muhammadfajri-tif <149763217+muhammadfajri-tif@users.noreply.github.com>
2024-05-21 08:05:44 +0700

    feat: add header for check os type

---
+++ includes/config.h
@@ -0,0 +1,21 @@
+#if !defined(config_H)
+#define config_H
+
+// Setup for OS
+#if defined(_WIN32)
+#define PLATFORM_NAME "windows" // Windows
+#elif defined(_WIN64)
+#define PLATFORM_NAME "windows" // Windows
+#elif defined(__CYGWIN__) && !defined(_WIN32)
+#define PLATFORM_NAME "windows" // Windows (Cygwin POSIX under Microsoft Window)
+#elif defined(__ANDROID__)
+#define PLATFORM_NAME "android" // load android first because it also linux
+#elif defined(__linux__)
+#define PLATFORM_NAME "linux" // linux
+#elif defined(linux)
+#define PLATFORM_NAME "linux" // linux
+#elif defined(__linux)
+#define PLATFORM_NAME "linux" // linux
+#endif
+
+#endif // currentos_H
